77df11574eb2edf85111f1839ce9106b9973bd63
Nikhil Chhabra <91406971+procrastinatedbread@users.noreply.github.com>
2023-06-24 03:00:21 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+
+<div>
+<img class="main" src="https://picsum.photos/350/350"/>
+<img src="https://picsum.photos/350/350" class="avatar">
+<h1>Hello Vanilla!</h1>
+ <p>Work Contact</p>
+  
+</div>
+`;
